3a625e1974c31e1fcd192d34329fab2a56ea2455
varoqr08 <alvaroqr@hotmail.com>
2019-06-08 12:13:51 -0600

    Update Populacion-P.sql

--- Postgres DataBase/Populacion-P.sql
+++ Postgres DataBase/Populacion-P.sql
@@ -1,4 +1,14 @@
-INSERT INTO Aeronave(Modelo, Capacidad, A_Economicos, A_Ejecutivos) VALUES(1,10,8,2)
+INSERT INTO Aeronave(Modelo, Capacidad, A_Economicos, A_Ejecutivos)
+		VALUES 
+		('747-400', 416, 250, 166),
+		('777-200', 300, 200, 100),
+		('A340-200', 440, 300, 145),
+		('767-300ER', 375, 225, 150),
+		('A330-300', 335, 200, 135),
+		('757-200', 289, 175, 114),
+		('737-700', 215, 150, 65),
+		('MD-80', 172, 100, 72),
+		('320', 150, 100, 150)
 INSERT INTO public.vuelo(
 	codigo, estado, c_economico, c_ejecutivo, f_salida, f_llegada, a_salida, a_llegada, millas, id_aeronave)
 	VALUES ('A00D', '0', 100, 200, '2019-06-04', '2019-06-05', 'AAA', 'AAB', 1000, 1);
